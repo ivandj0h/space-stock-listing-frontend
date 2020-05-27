62c003e29caeca98ff91fbd1330c887b72621b8b
ivandi1980 <ivandi.djoh@gmail.com>
2020-05-27 10:26:06 +0700

    remove component card

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.scss";
 import { Container, Row, Col } from "react-bootstrap";
-import MyCard from "./components/MyCard";
+
 
 function App() {
   return (
     <Container>
       <Row>
         <Col>
-          <MyCard />
+          
         </Col>
       </Row>
     </Container>
